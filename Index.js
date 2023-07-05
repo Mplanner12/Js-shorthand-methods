5440a4b12b24eaed1ce09a56d81a6c0aac60aaca
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-07-05 21:20:08 +0100

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,14 @@
+// javascript shorthand methods 
+
+// forEach method
+let numbers = [1,2,3,4,5,6]
+numbers.forEach(number =>{
+    const result = number
+    console.log(result)
+})
+
+let fruits = ["mango", "apple", "date", "banana"]
+fruits.forEach(fruit =>{
+    collection = fruit
+    console.log(collection)
+})
